3a04780d1fb7ff820ca7c8a0d84fcc6987558c72
Augant <antoniooaugustoo@gmail.com>
2024-09-14 18:00:36 -0300

    Projeto base funcionando

--- projeto_app/urls.py
+++ projeto_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from projeto_app.views import index, form, list
+from projeto_app.views import index, form, list, edit
 
 urlpatterns = [
     path('', index),
     path('form/', form, name='form'),
-    path('list/', list),
+    path('list/', list, name='list'),
+    path('edit/<int:contato_id>/', edit, name='edit')
 
 ]
--- projeto_app/views.py
+++ projeto_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,  get_object_or_404
 from projeto_app.models import Contato
+from django.http import HttpResponse
 
 
 def index(request):
@@ -20,4 +21,33 @@ def form(request):
     return render(request, "partial/form.html", {'nome': nome})
 
 def list(request):
-    return render(request, "partial/list.html")
+    query = request.GET.get('q')
+    if query:
+        contatos = Contato.objects.filter(nome__icontains=query)
+    else:
+        contatos = Contato.objects.all()
+
+    if request.method == 'POST':  # Checa se a requisição é para deletar
+        contato_id = request.POST.get('contato_id')
+        contato = get_object_or_404(Contato, id=contato_id)
+        contato.delete()
+        return redirect('list')
+
+    return render(request, 'partial/list.html', {'contatos': contatos, 'query': query})
+
+def edit(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+    
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        data_nascimento = request.POST.get('data-nascimento')
+        
+        if nome and email and data_nascimento:
+            contato.nome = nome
+            contato.email = email
+            contato.data_nascimento = data_nascimento
+            contato.save()
+            return redirect('list')
+    
+    return render(request, 'partial/edit.html', {'contato': contato})
